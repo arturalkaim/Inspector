a42ab9197ab7f9bf6b772d615df193f6b12f9a9c
Artur Alkaim <arturalkaim@gmail.com>
2014-03-09 23:05:18 +0000

    Commit n 2

--- Inspector/src/ist/meic/pa/Inspector.java
+++ Inspector/src/ist/meic/pa/Inspector.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Modifier;
 public class Inspector {
 	private boolean go = true;
 	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	
+
 	public void inspect(Object object) {
 		try {
 
@@ -23,50 +23,68 @@ public class Inspector {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
 
 	private Object eval(String cmd) {
-		if(cmd.equalsIgnoreCase("exit") || cmd.equalsIgnoreCase("q")){
-			go=false;
+		if (cmd.equalsIgnoreCase("exit") || cmd.equalsIgnoreCase("q")) {
+			go = false;
 		}
-		System.out.println(cmd);
+		System.err.println(cmd);
 		return cmd;
 	}
 
 	private void print(Object eval) {
-		
-	}
 
+	}
 
 	private String read() {
 		String cmd = "";
-		System.out.print(">");
+		System.err.print(">");
 		try {
-		
+
 			cmd = in.readLine();
-			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return cmd;
-		
+
 	}
 
 	private void printData(Object object) throws IllegalArgumentException,
-			IllegalAccessException {
+			IllegalAccessException, InstantiationException {
 		Class<?> cl = object.getClass();
-		System.out.println(object.toString() + " is an instance of class " + cl.getCanonicalName());
-		System.out.println("extends " + cl.getGenericSuperclass());
+		System.err.println(object.toString() + " is an instance of class "
+				+ cl.getCanonicalName());
+		Class<?> sup = cl.getSuperclass();
+
+		if (!(sup.isInstance(new Object()))) {
+			System.err.println("extends " + sup);
+			System.err.println("----------");
+
+			for (Field f : sup.getDeclaredFields()) {
+				f.setAccessible(true);
+				System.err.println(Modifier.toString(f.getModifiers()) + " "
+						+ f.getType().getCanonicalName() + " " + f.getName()
+						+ " = " + f.get(object).toString());
+			}
+
+
+		} else
+			System.err.println("----------");
 
 		for (Field f : cl.getDeclaredFields()) {
 			f.setAccessible(true);
-			System.out.println(Modifier.toString(f.getModifiers()) + " " + f.getType().getCanonicalName() + " " + f.getName() + " = " + f.get(object).toString());
+			System.err.println(Modifier.toString(f.getModifiers()) + " "
+					+ f.getType().getCanonicalName() + " " + f.getName()
+					+ " = " + f.get(object).toString());
 		}
 	}
 }
