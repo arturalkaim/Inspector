61ae0663fb98d5b35945cd1a8d7533332b6549f7
Artur Alkaim <arturalkaim@gmail.com>
2014-03-13 23:24:04 +0000

    Minimo feito

--- Inspector/src/ist/meic/pa/Inspector.java
+++ Inspector/src/ist/meic/pa/Inspector.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class Inspector {
 	private boolean go = true;
@@ -27,21 +30,78 @@ public class Inspector {
 		} catch (InstantiationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (ReflectiveOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RuntimeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 	}
 
 	private Object eval(String cmd) throws IllegalArgumentException,
-			IllegalAccessException, InstantiationException {
+			IllegalAccessException, InstantiationException,
+			ReflectiveOperationException, RuntimeException {
 		if (cmd.equalsIgnoreCase("exit") || cmd.equalsIgnoreCase("q")) {
 			go = false;
 		} else if (cmd.startsWith("i ")) {
 			InspectCommand(cmd);
 		} else if (cmd.startsWith("m ")) {
 			ModifieCommand(cmd);
+		} else if (cmd.startsWith("c ")) {
+			CallCommand(cmd);
 		} else
 			System.err.println(cmd);
 		return cmd;
+	}
+
+	private void CallCommand(String cmd) throws SecurityException,
+			IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, NoSuchMethodException {
+		String[] args = cmd.split(" ");
+		if (args.length < 2) {
+			System.err
+					.println("The use of this command is:  m <name> [<value> <value> <value> ...] ");
+		}
+		Class<?> godClass = god.getClass();
+		try {
+			invokeMethod(godClass, args);
+		} catch (NoSuchMethodException e) {
+			if (godClass.isInstance(Object.class)) {
+				throw e;
+			} else {
+				invokeMethod(godClass.getSuperclass(), args);
+			}
+		}
+
+	}
+
+	private void invokeMethod(Class<?> godClass, String[] args)
+			throws NoSuchMethodException, SecurityException,
+			IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException {
+		Method m;
+		if (args.length == 2) {
+
+			m = godClass.getMethod(args[1]);
+
+			m.setAccessible(true);
+			m.invoke(god);
+		} else if (args.length > 2) {
+			Class<Integer>[] arg = new Class[args.length - 2];
+			Arrays.fill(arg, Integer.TYPE);
+			m = godClass.getDeclaredMethod(args[1], arg);
+			m.setAccessible(true);
+			Integer[] params = new Integer[args.length - 2];
+			for (int i = 2; i < args.length; i++) {
+				params[i - 2] = Integer.parseInt(args[i]);
+			}
+			Object res = m.invoke(god, (Object[]) params);
+			if (res != null) {
+				System.err.println(res.toString());
+			}
+		}
 	}
 
 	private void ModifieCommand(String cmd) throws IllegalArgumentException,
@@ -53,19 +113,41 @@ public class Inspector {
 		}
 		Class<?> godClass = god.getClass();
 
-		for (Field f : godClass.getDeclaredFields()) {
+		for (Field f : godClass.getSuperclass().getDeclaredFields()) {
 			if (f.getName().equals(args[1])) {
 				f.setAccessible(true);
-				
+
 				setVar(f, args[2]);
-				
-				if (f.getModifiers() != 0)
+
+				if (f.getModifiers() != 0) {
 					System.err.println(Modifier.toString(f.getModifiers())
 							+ " " + f.getType().getCanonicalName() + " "
 							+ f.getName() + " = " + f.get(god).toString());
-				else
+					return;
+				} else {
 					System.err.println(f.getType().getCanonicalName() + " "
 							+ f.getName() + " = " + f.get(god).toString());
+					return;
+				}
+			}
+		}
+
+		for (Field f : godClass.getDeclaredFields()) {
+			if (f.getName().equals(args[1])) {
+				f.setAccessible(true);
+
+				setVar(f, args[2]);
+
+				if (f.getModifiers() != 0) {
+					System.err.println(Modifier.toString(f.getModifiers())
+							+ " " + f.getType().getCanonicalName() + " "
+							+ f.getName() + " = " + f.get(god).toString());
+					return;
+				} else {
+					System.err.println(f.getType().getCanonicalName() + " "
+							+ f.getName() + " = " + f.get(god).toString());
+					return;
+				}
 			}
 		}
 	}
@@ -166,9 +248,9 @@ public class Inspector {
 		for (Field f : cl.getDeclaredFields()) {
 			f.setAccessible(true);
 			if (f.getModifiers() != 0)
-				System.err.println(Modifier.toString(f.getModifiers())
-						+ " " + f.getType().getCanonicalName() + " "
-						+ f.getName() + " = " + f.get(god).toString());
+				System.err.println(Modifier.toString(f.getModifiers()) + " "
+						+ f.getType().getCanonicalName() + " " + f.getName()
+						+ " = " + f.get(god).toString());
 			else
 				System.err.println(f.getType().getCanonicalName() + " "
 						+ f.getName() + " = " + f.get(god).toString());
--- InspectorTest/src/ist/meic/pa/B.java
+++ InspectorTest/src/ist/meic/pa/B.java
@@ -3,4 +3,8 @@ package ist.meic.pa;
 public class B {
 	private String c = "Hello World";
 	protected int d;
+	
+	private int h(int a, int b, int c){
+		return a + b + c;
+	}
 }
